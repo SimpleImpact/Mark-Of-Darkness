a89dcbcea4104784bbedbf5b6a8b0bd5cfd67c1c
SimpleImpact <gwalters5926@gmail.com>
2024-11-04 07:14:31 -0600

    Better Dash Ability

--- Scripts/Movement.gd
+++ Scripts/Movement.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @onready var _animated_sprite = $Player
 @export var rotation_speed = 1.5
 var rotation_direction = 0
+var dash = true
 
 var speed = 400
 var target
@@ -31,9 +32,13 @@ func _process(_delta: float) -> void:
 	
 func _physics_process(_delta: float) -> void:
 	get_input()
-	if Input.is_action_pressed("Mouse"):
-		if position.distance_to(target) > 10 and position.distance_to(target) < 0.1:
-			velocity = position.direction_to(target) * speed * 10
+	if Input.is_action_pressed("Mouse") and dash:
+		if position.distance_to(target) > 50:
+			print(self.position.distance_to(target))
+			velocity = position.direction_to(target) * self.position.distance_to(target) * Engine.get_frames_per_second()/4
+			print(Engine.get_frames_per_second())
+			start_timer()
+			dash = false
 	move_and_slide()
 
 func get_input():
@@ -41,3 +46,10 @@ func get_input():
 	velocity = input_direction * speed * 1
 	target = Vector2(0, 0)
 	target = get_global_mouse_position()
+
+func start_timer():
+	var timer = get_tree().create_timer(1.0)
+	timer.timeout.connect(timeout_function)
+
+func timeout_function():
+	dash = true
