47eb06a8265f7bdc56d262236f873bcd8343e985
SimpleImpact <109104323+SimpleImpact@users.noreply.github.com>
2024-11-22 14:00:40 -0600

    update

--- Scripts/Attack Handler.gd
+++ Scripts/Attack Handler.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@onready var slash = $Slash
 var cooldown = true
+var slashObject: PackedScene = preload("res://Objects/Attacks/slash.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,17 +10,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if Input.is_action_pressed("Attack") and cooldown:
-		slash.play("Slash")
-		slash.rotation_degrees = rad_to_deg(get_angle_to(get_global_mouse_position())) + 90
-		cooldown = false
-		start_timer()
+	slash()
 	pass
 
-func start_timer():
-	var timer = get_tree().create_timer(1.0)
-	timer.timeout.connect(timeout_function)
 
-func timeout_function():
-	cooldown = true
-	pass
+	
+func slash():
+	var slash = slashObject.instantiate()
