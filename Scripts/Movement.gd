22438254e98f86f5eb3dd8420a7b9629b38d641e
SimpleImpact <109104323+SimpleImpact@users.noreply.github.com>
2024-12-04 13:58:53 -0600

    Update

--- Scripts/Movement.gd
+++ Scripts/Movement.gd
@@ -17,8 +17,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	
-	playerInfo.playerPos = _animated_sprite.position
-	
 	if Input.is_action_pressed("Down") and Input.is_action_pressed("Left") and Input.is_action_pressed("Up") and Input.is_action_pressed("Right"):
 		_animated_sprite.play("Rouge Idle")
 	
