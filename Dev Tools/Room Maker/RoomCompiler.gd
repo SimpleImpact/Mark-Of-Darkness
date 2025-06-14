510d455fe89e93cea5edf33b33cc7cba88677664
Simpleimpact <gwalters5926@gmail.com>
2025-06-14 11:04:07 -0500

    Cleaned Directory and made a custom room builder

--- Dev Tools/Room Maker/RoomCompiler.gd
+++ Dev Tools/Room Maker/RoomCompiler.gd
@@ -4,14 +4,14 @@ extends Node2D
 var room:Array
 ###  Tile Cords           Atlas Cords
 #               \        /
-### Outputs [[c(1, 0), a(0, 0)], [(2, 0), (0, 1)], ...]
+### Outputs [[c1, 0), a0, 0)], [c2, 0), a0, 1)], ...]
 
 func _ready() -> void:
 	for r in 8:
 		for e in 8:
-			var index = tilemap.get_cell_atlas_coords(Vector2i(e-4, r-4))
+			var index = tilemap.get_cell_atlas_coords(Vector2i(e, r))
 			if index != Vector2i(-1, -1):
-				room.append(["c" + str(e) + ", " + str(r) +")", "a" + str(index.x) + ", " + str(index.y) +")",])
+				room.append(["c" + str(e) + ", " + str(r) +")", "a" + str(index.x) + ", " + str(index.y) +")"])
 	print(room)
 
 	
