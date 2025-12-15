ce50b7bbc5204d3ab19219fe78b511435c9f83f9
SimpleImpact <gwalters5926@gmail.com>
2025-12-15 11:30:22 -0600

    Refactor bow attack and update cobble tileset
    Refactored the bow attack to use a new SpriteFrames resource and updated the bow scene and script for improved animation handling. Replaced the cobble tile texture and updated the tileset and map generation scripts to use the new tile indices and sizes. Removed the old door sprite and import, and added new cobbleV2 assets.

--- Scripts/Attacks/bow.gd
+++ Scripts/Attacks/bow.gd
@@ -1,12 +1,15 @@
-extends AnimatedSprite2D
+extends Node2D
 
 var cooldown = true
 
+@onready var sprite = $AnimatedSprite2D
+
 func _process(_delta: float) -> void:
 	if Input.is_action_pressed("Attack") and cooldown:
+		print("Bow Attack")
 		look_at(get_global_mouse_position())
-		rotation_degrees += 90
-		play("shoot")
+		#rotation_degrees += 90
+		sprite.play("fire")
 		cooldown = false
 		start_timer()
 
