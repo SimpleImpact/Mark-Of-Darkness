75a7bf0b62840b7b8039885bd3eb89e6a9747dff
Simpleimpact <gwalters5926@gmail.com>
2026-01-17 00:52:08 -0600

    arrows hit walls now (but it works this time)

--- Scripts/Attacks/projectiles/arrow.gd
+++ Scripts/Attacks/projectiles/arrow.gd
@@ -4,9 +4,10 @@ var yFactor = sin(rotation)
 var xFactor = cos(rotation)
 
 @export var baseSpeed = 1500
-
+var tweenSpriteScale : Tween
+var tweenSpritePos : Tween
 @onready var sprite = $Sprite2D
-	
+
 func _physics_process(_delta: float) -> void:
 	yFactor = sin(rotation)
 	xFactor = cos(rotation)
@@ -14,17 +15,15 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func hitWall():
-	print("hit wall")
 	set_physics_process(false)
-	var ogScale = sprite.scale
-	for i in 10:
-		sprite.scale = ogScale * 1/i
+	tweenSpriteScale = create_tween()
+	tweenSpritePos = create_tween()
+	tweenSpriteScale.tween_property(sprite, "scale", Vector2(0, 0), 2)
+	tweenSpritePos.tween_property(sprite, "position", Vector2(100, 0), 2)
+	await tweenSpritePos.finished
+	queue_free()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("hit smth")
-	if body.has_meta("Enemy"):
-		print("Arrow hit enemy")
-	else:
-		print("hit not an enemy")
+	if body.get_class() == "TileMapLayer":
 		hitWall()
